e68c1ee0aa97f93e2b5229f512547056195f4587
PAGESBen <84289892+PAGESBen@users.noreply.github.com>
2022-09-25 18:09:28 +0200

    correction erreur

--- models/Quote.js
+++ models/Quote.js
@@ -4,7 +4,7 @@ const quoteSchema = mongoose.Schema({
   Quote: { type: String, required: true },
   Author: { type: String, required: true },
   imageUrl: { type: String, required: false },
-  proposedBy: { type: string, required: false }
+  proposedBy: { type: String, required: false }
 });
 
 module.exports = mongoose.model('quote', quoteSchema);
