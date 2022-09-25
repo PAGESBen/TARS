e4452cb88fc1d0f11c1aea8a3871d512ae3e396c
PAGESBen <84289892+PAGESBen@users.noreply.github.com>
2022-09-25 17:37:40 +0200

    commentaire sur les routes quote

--- routes/quote.js
+++ routes/quote.js
@@ -6,11 +6,11 @@ const multer = require('../middleware/multer-config');
 
 const quoteCtrl = require('../controllers/quote');
 
-router.get('/', auth, quoteCtrl.getAllQuote );
-router.get('/:id', auth, quoteCtrl.getOneQuote );
-router.post('/', auth, multer, quoteCtrl.postOneSQuote );
-router.put('/:id', auth, multer, quoteCtrl.modifyOneQuote );
-router.delete('/:id', auth, quoteCtrl.deleteOneQuote ); 
-router.post('/:id/like', auth, quoteCtrl.like );
+// router.get('/', auth, quoteCtrl.getAllQuote );
+// router.get('/:id', auth, quoteCtrl.getOneQuote );
+// router.post('/', auth, multer, quoteCtrl.postOneSQuote );
+// router.put('/:id', auth, multer, quoteCtrl.modifyOneQuote );
+// router.delete('/:id', auth, quoteCtrl.deleteOneQuote ); 
+// router.post('/:id/like', auth, quoteCtrl.like );
 
 module.exports = router;
